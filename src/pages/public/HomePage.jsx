bba4bd66e4858cd6eac59a1cccbb016701e5aa89
Wrap & Roll POS <wraproll@pos.app>
2026-09-05 09:27:53 +0300

    feat: animate menu heading characters

--- src/pages/public/HomePage.jsx
+++ src/pages/public/HomePage.jsx
@@ -141,10 +141,7 @@ export default function HomePage() {
         { to: '-2.75rem', ease: 'outExpo', duration: 600 },
         { to: 0, ease: 'outBounce', duration: 800, delay: 100 },
       ],
-      rotate: {
-        from: '-1turn',
-        delay: 0,
-      },
+      rotate: { from: '-1turn', delay: 0 },
       delay: stagger(50),
       ease: 'inOutCirc',
       loopDelay: 1000,
